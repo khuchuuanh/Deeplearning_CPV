d74d064d3cf25617e74785d7154de31dc015e45f
KhucHuuAnh <khuchuuanh@gmail.com>
2023-05-21 17:19:49 +0700

    data process

--- Driving_car/driving_data.py
+++ Driving_car/driving_data.py
@@ -2,6 +2,12 @@ import cv2
 import random
 import numpy as np
 import csv
+import os
+from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+
+
+
 xs = []
 ys = []
 
@@ -28,4 +34,26 @@ val_ys = ys[-int(len(xs) * 0.2):]
 
 num_train_images = len(train_xs)
 num_val_images = len(val_xs)
+
+def read_and_process_images(folder_path, target_shape):
+    images = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            image_path = os.path.join(folder_path, filename)
+            try:
+                image = Image.open(image_path)
+                image = image.resize(target_shape[:2])
+                image_array = img_to_array(image)
+                images.append(image_array)
+            except Exception as e:
+                print(f"Error reading image '{filename}': {str(e)}")
+    
+    images = np.array(images) 
+    return images
+
+folder_path = "driving_data/driving_dataset"
+target_shape = (66, 200, 3)
+images = read_and_process_images(folder_path, target_shape)
+images = images/255.0
+
 
