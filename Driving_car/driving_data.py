257a1951501a583912523dd640bb0f1de2e60259
KhucHuuAnh <khuchuuanh@gmail.com>
2023-05-22 21:44:30 +0700

    this code about load dataset for project

--- Driving_car/driving_data.py
+++ Driving_car/driving_data.py
@@ -2,25 +2,22 @@ import cv2
 import random
 import numpy as np
 import csv
-import os
-from PIL import Image
-from tensorflow.keras.preprocessing.image import img_to_array
-
-
-
 xs = []
 ys = []
 
+#points to the end of the last batch
 train_batch_pointer = 0
 val_batch_pointer = 0
+
+#read data.txt
 with open("driving_data/steering angle.csv") as f:
     reader = csv.reader(f)
     for line in reader:
-        xs.append("driving_data/" + line[0])
+        xs.append("driving_data/driving_dataset/data/" + line[0])
         ys.append(float(line[1]) * 3.14159265 / 180)
 
+#get number of images
 num_images = len(xs)
-#print("num_images : ", num_images)
 #shuffle list of images
 c = list(zip(xs, ys))
 random.shuffle(c)
@@ -35,25 +32,26 @@ val_ys = ys[-int(len(xs) * 0.2):]
 num_train_images = len(train_xs)
 num_val_images = len(val_xs)
 
-def read_and_process_images(folder_path, target_shape):
-    images = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            image_path = os.path.join(folder_path, filename)
-            try:
-                image = Image.open(image_path)
-                image = image.resize(target_shape[:2])
-                image_array = img_to_array(image)
-                images.append(image_array)
-            except Exception as e:
-                print(f"Error reading image '{filename}': {str(e)}")
-    
-    images = np.array(images) 
-    return images
+def LoadTrainBatch(batch_size):
+    global train_batch_pointer
+    x_out = []
+    y_out = []
+    for  i in range(0, batch_size):
+        x_out.append(cv2.resize(cv2.imread(f'{train_xs[(train_batch_pointer + i) % num_train_images]}.jpg')[-150:], (200, 66)) / 255.0)
+        y_out.append([train_ys[(train_batch_pointer + i) % num_train_images]])
 
-folder_path = "driving_data/driving_dataset"
-target_shape = (66, 200, 3)
-images = read_and_process_images(folder_path, target_shape)
-images = images/255.0
+    train_batch_pointer +=  batch_size
+    return  x_out, y_out
+
+def LoadValBatch(batch_size):
+    global val_batch_pointer
+    x_out = []
+    y_out = []
+    for i in range(0, batch_size):
+        x_out.append(cv2.resize(cv2.imread(f'{val_xs[(val_batch_pointer + i) % num_val_images]}.jpg')[-150:], (200, 66)) / 255.0)
+        y_out.append([val_ys[(val_batch_pointer + i) % num_val_images]])
+    val_batch_pointer += batch_size
+    return x_out, y_out
+
 
 
