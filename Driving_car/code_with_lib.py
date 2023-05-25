7286eb1f044326df7ce9eb9aaac5a2d00742d61b
KhucHuuAnh <khuchuuanh@gmail.com>
2023-05-25 19:51:23 +0700

    this code using library to code for project

--- Driving_car/code_with_lib.py
+++ Driving_car/code_with_lib.py
@@ -4,6 +4,8 @@ import os
 from PIL import Image
 import numpy as np
 from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+
 
 label_data = pd.read_csv('driving_data/steering angle.csv')
 
@@ -35,6 +37,68 @@ test_labels = np.array(test_labels)
 
 train_images = train_images.reshape((-1, 66,200,3))
 test_images = test_images.reshape((-1, 66,200,3))
+
+
+# building model
+
+model = keras.models.Sequential()
+model.add(tf.keras.Input(shape = (66,200,3)))
+
+model.add(keras.layers.BatchNormalization())
+
+
+model.add(keras.layers.Conv2D(24,5,2 ))
+
+model.add(keras.layers.Conv2D(36,5,2))
+
+model.add(keras.layers.Conv2D(48,5,2))
+
+model.add(keras.layers.Conv2D(64,3,1))
+
+model.add(keras.layers.Conv2D(64,3,1))
+
+model.add(keras.layers.Flatten())
+
+model.add(keras.layers.Dense(1164))
+model.add(keras.layers.Dropout(0.8))
+
+model.add(keras.layers.Dense(100))
+model.add(keras.layers.Dropout(0.8))
+
+model.add(keras.layers.Dense(50))
+model.add(keras.layers.Dropout(0.8))
+
+model.add(keras.layers.Dense(10))
+model.add(keras.layers.Dropout(0.8))
+
+model.add(keras.layers.Dense(1))
+
+model.summary()
+
+
+callback_path = './checkfolder/checkpoint.ckpt'
+
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    callback_path,
+    save_weights_only= True,
+    save_best_only= True,
+    monitor= "val_loss",     # theo dõi và quyết định  lưu checkpoint tốt nhất
+    verbose= 1
+) 
+
+
+model.compile(optimizer = 'adam',
+              loss = tf.keras.losses.SparseCategoricalCrossentropy(),
+              metric = ["Accuracy"]
+              )
+
+
+history_data = model.fit(train_images, train_labels,
+                         validation_data = (test_images, test_labels),
+                         batch_size = 100 ,
+                         epochs = 30,
+                         callbacks = [checkpoint_callback]
+                         )
 
 
 
