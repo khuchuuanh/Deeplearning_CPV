33ade22d793fdded2ada2004472b057ab1c0059f
KhucHuuAnh <khuchuuanh@gmail.com>
2023-05-29 23:02:44 +0700

    code with lib

--- Driving_car/code_with_lib.py
+++ Driving_car/code_with_lib.py
@@ -5,6 +5,8 @@ from PIL import Image
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
+import cv2
+from subprocess import call
 
 
 label_data = pd.read_csv('driving_data/steering angle.csv')
@@ -102,6 +104,34 @@ history_data = model.fit(train_images, train_labels,
 
 
 
+windows = False
+if os.name == "nt":
+    windows = True
 
+sess  = tf.InteractiveSession()
+saver = tf.train.Saver()
+saver.restore(sess, './save/model.ckpt')
+img = cv2.imread('./steering_wheel_image.jpg',0)
+rows, cols = img.shape
+
+smoothed_angle = 0
+
+cap =cv2.VideoCapture(0)
+while(cv2.waitKey(10) != ord('q')):
+    ret, frame = cap.read()
+    image = cv2.resize(frame, (200, 66))/255.0
+    degrees = model.y.eval(feed_dict = {model.x: [image], model.keep_prob:1.0})[0][0] * 180/3.14159265
+    if not windows:
+        call('clear')
+    print("Predicted steering angle : " + str(degrees) + "degrees")
+    cv2.imshow('frame', frame)
+
+    smoothed_angle += 0.2*pow(abs((degrees - smoothed_angle)),2.0/3.0) *(degrees - smoothed_angle)/ abs(degrees - smoothed_angle)
+    M = cv2.getRotationMatrix2D((cols/2, rows/2), -smoothed_angle,1)
+    dst = cv2.warpAffine(img, M, (cols, rows))
+    cv2.imshow("steering wheel", dst)
+
+cap.release()
+cv2.destroyAllWindows()
 
 
