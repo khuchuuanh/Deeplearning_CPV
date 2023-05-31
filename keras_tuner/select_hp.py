553682070e40d16750426b5353f8c68499dc915f
KhucHuuAnh <khuchuuanh@gmail.com>
2023-05-31 19:28:14 +0700

    using keras tuner to choose best HP for deep learning model

--- keras_tuner/select_hp.py
+++ keras_tuner/select_hp.py
@@ -95,3 +95,92 @@ plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
+
+# building model for keras tuner
+
+def build_model(hp):
+  model =  keras.models.Sequential()
+  model.add(tf.keras.Input(shape = (32,32,3)))
+
+  filters = hp.Int('filters', min_value  = 32, max_value = 128, step = 32)
+  kernel_size = hp.Choice("kernel_size", values = [3,5])
+  hp_unit = hp.Int('units', min_value = 32, max_value = 512, step = 32)
+  hp_learning_rate = hp.Choice("learning_rate", values = [1e-2, 1e-3, 1e-4])
+
+  model.add(keras.layers.Conv2D(filters, kernel_size, 1, padding = 'same', activation = 'relu'))
+  model.add(keras.layers.Dropout(0.5))
+  model.add(keras.layers.BatchNormalization())
+
+  model.add(keras.layers.Conv2D(filters,kernel_size,1, padding = 'same' ,activation = 'relu'))
+  model.add(keras.layers.Dropout(0.5))
+  model.add(keras.layers.BatchNormalization())
+
+  model.add(keras.layers.MaxPooling2D(2))
+
+  model.add(keras.layers.Conv2D(128,3,1, padding = 'same', activation = 'relu'))
+  model.add(keras.layers.Dropout(0.5))
+  model.add(keras.layers.BatchNormalization())
+
+  model.add(keras.layers.Conv2D(128,3,1, padding = 'same' ,activation = 'relu')) 
+  model.add(keras.layers.BatchNormalization())
+
+  model.add(keras.layers.MaxPooling2D(2))
+  model.add(keras.layers.Flatten())
+
+  model.add(keras.layers.Dense(hp_unit, activation = 'relu'))
+
+  model.add(keras.layers.Dense(10, activation = 'softmax'))
+
+  model.compile(optimizer = keras.optimizers.Adam(learning_rate = hp_learning_rate),
+                loss = tf.keras.losses.SparseCategoricalCrossentropy(),
+                metrics = ["Accuracy"])
+  
+  return model
+
+  # các tham số hiệu chỉnh
+  # filters = hp.Int('filters', min_value  = 32, max_value = 128, step = 32)
+  # kernel_size = hp.Choice("kernel_size", values = [3,5])
+  # hp_unit = hp.Int('units', min_value = 32, max_value = 512, step = 32)
+  # hp_learning_rate = hp.Choice("learning_rate", values = [1e-2, 1e-3, 1e-4])
+
+tuner = kt.Hyperband(build_model,
+                     objective='val_Accuracy',
+                     max_epochs=10,
+                     factor=3,
+                     directory='./keras_tuner1',
+                     project_name='Project_2')
+  
+tuner.search_space_summary()
+
+stop_early = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 5)
+tuner.search(train_images, train_labels, epochs=50, validation_split=0.2, callbacks=[stop_early])
+best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
+
+print('The best unit is : ',best_hps.get("units"))
+print("The best learning rate is :",best_hps.get("learning_rate"))
+print("The best number of filters is : ",(best_hps.get("filters")))
+print("The best size of kernel is :  ",(best_hps.get("kernel_size")))
+
+model = tuner.hypermodel.build(best_hps)
+history = model.fit(train_images, train_labels, epochs=50, validation_split=0.2)
+val_acc_per_epoch = history.history['val_Accuracy']
+best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
+print('Best epoch: %d' % (best_epoch,))
+
+model  = tuner.hypermodel.build(best_hps)
+history_data = model.fit(train_images, train_labels, epochs=best_epoch, validation_split=0.2)
+
+# train loss and validation loss visualization after using selected HP
+plt.plot(history_data.history['loss'], label = 'train_loss')
+plt.plot(history_data.history['val_loss'], label = 'val_loss')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+plt.plot(history_data.history['Accuracy'], label = 'train_Accuracy')
+plt.plot(history_data.history['val_Accuracy'], label = 'val_Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
