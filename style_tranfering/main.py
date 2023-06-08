1b3d73234073b98f81f105f0014525b717edcb9a
KhucHuuAnh <khuchuuanh@gmail.com>
2023-06-08 22:28:26 +0700

    using model like black box to create new image

---
+++ style_tranfering/main.py
@@ -0,0 +1,100 @@
+import os 
+import tensorflow as tf
+import IPython.display as display
+
+import matplotlib.pyplot as plt
+
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12,12)
+mpl.rcParams['axes.grid'] = False
+import tensorflow_hub as hub
+import numpy as np
+import  PIL.Image
+import time
+import functools
+
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
+
+
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+
+def imshow(image, title = None):
+  plt.imshow(image)
+  if title:
+    plt.title(title)
+
+
+content_path = ('/content/drive/MyDrive/style tranfer/content.jpg')
+style_path = ('/content/drive/MyDrive/style tranfer/style4.jpg')
+content_image = load_img(content_path)
+style_image = load_img(style_path)
+
+plt.subplot(1, 2, 1)
+imshow(content_image[0], 'Content Image')
+plt.subplot(1, 2, 2)
+imshow(style_image[0], 'Style Image')
+
+
+hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
+tensor_to_image(stylized_image)
+
+
+content_layers = ['block5_conv2']
+
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1']
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+
+def vgg_layers(layer_names):
+  vgg = tf.keras.applications.VGG19(include_top = False,
+                                    weights = 'imagenet')
+  vgg.trainable  = False
+  outputs = [vgg.get_layer(name).output for name in layer_names]
+  model = tf.keras.Model([vgg.input],outputs)
+  return model
+
+
+style_extractor = vgg_layers(style_layers)
+style_outputs = style_extractor(style_image*255)
+
+for name, output in zip(style_layers, style_outputs):
+  print(name)
+  print("  shape: ", output.numpy().shape)
+  print("  min: ", output.numpy().min())
+  print("  max: ", output.numpy().max())
+  print("  mean: ", output.numpy().mean())
+  print()
+
+
+def gram_matrix(input_tensor):
+  result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+  input_shape = tf.shape(input_tensor)
+  num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+  return result/(num_locations)
