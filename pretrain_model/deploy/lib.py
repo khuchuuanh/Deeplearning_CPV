1aa5cdd9054ffa39b7a0be9a95b7600145f1c370
KhucHuuAnh <khuchuuanh@gmail.com>
2023-06-13 15:27:41 +0700

    lib for project which is about using pretrain model and deploy on the website

---
+++ pretrain_model/deploy/lib.py
@@ -0,0 +1,14 @@
+import glob 
+import os.path as osp
+import random
+import numpy as np
+import json 
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+from torchvision import models, transforms
+from tqdm import tqdm
